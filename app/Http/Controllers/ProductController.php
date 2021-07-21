f56c5f27d90afbd70ff98f29eb0d70d3009eb704
mdssantos <matheusmep@gmail.com>
2021-07-21 15:52:02 -0400

    firts commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = ['Product 01', 'Product 02', 'Product 03'];
+        return $products;
+    }
+
+    public function show($id) {
+        return "Exibindo o produto de id: {$id}";
+    }
+}
